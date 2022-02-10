f2fd4ba52df59129563deaf991c7127acff49889
askmary <mariana.reinaldo2003@gmail.com>
2022-02-09 21:29:31 -0300

    react testando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, {Component} from 'react'
+import "./App.css"
+export default class App extends Component{
+  Arrow = (x, y) =>{
+    return(x + y)
+  }
+  render(){
+    return(
+      <h1 className="one">Meu gato mais velho tem {this.Arrow(10,6)} anos</h1>
+    )
+  } 
+} 
